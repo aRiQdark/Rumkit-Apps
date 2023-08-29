4edb8c83a3d21f3b78a0685c8677560d86b66b1d
Muhammad Ariq Pratama <muhammadariq105@gmail.com>
2023-08-29 10:31:34 +0700

    update tampilan

--- lib/view/beranda.dart
+++ lib/view/beranda.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -33,29 +34,40 @@ class Beranda extends GetView<rumkitcontroller> {
                     scrollDirection: Axis.horizontal,
                     itemCount: 3,
                     itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin:
-                                EdgeInsets.only(left: 16, right: 16, top: 20),
-                            height: 149,
-                            width: 250,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12),
-                                color: Color.fromARGB(255, 187, 182, 182)),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 26),
-                            child: Text(
-                              "Berita terbaru dari covid 19 \n jangan keluar keluar",
-                              style: TextStyle(fontWeight: FontWeight.bold),
+                      return CarouselSlider(
+
+                        items: [
+                          Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin:
+                                  EdgeInsets.only(left: 0, right: 16, top: 20),
+                              height: 149,
+                              width: 250,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  color: Color.fromARGB(255, 187, 182, 182)),
                             ),
-                          )
-                        ],
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 26),
+                              child: Text(
+                                "Berita terbaru dari covid 19 \n jangan keluar keluar",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            )
+                          ],
+                        ),
+                        ], options: CarouselOptions(
+                          aspectRatio: 19/12,
+                          autoPlayCurve: Curves.decelerate,
+                          autoPlay: true,
+
+                        ),
+                         
                       );
                     },
                   ),
@@ -107,34 +119,59 @@ class Beranda extends GetView<rumkitcontroller> {
                     scrollDirection: Axis.vertical,
                     itemCount: snapshot.data?.length,
                     itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin:
-                                EdgeInsets.only(left: 16, right: 16, top: 20),
-                            height: 149,
-                            width: 350,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12),
-                                color: Color.fromARGB(255, 187, 182, 182)),
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                      return Container(
+                        margin: EdgeInsets.only(left: 16, right: 16, top: 20),
+                        height: 149,
+                        width: 350,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Color.fromARGB(255, 231, 220, 220)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 30,
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.start,
                                 children: [
-                                  Text("${API?[index].name}"),
-                                  Text("${API?[index].address}"),
-                                  Text("${API?[index].province}"),
-                                  API?[index].phone != null
-                                      ? Text("${API?[index].phone}")
-                                      : Text("Tidak ada nomor hp"),
-                                  Text("${API?[index].region}")
+                                  Container(
+                                    height: 50,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        color: Colors.amber),
+                                  ),
+                                  SizedBox(width: 10,),
+                                  Expanded(
+                                    child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("${API?[index].name}",overflow: TextOverflow.ellipsis,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
+                                        // RichText(
+                                        //   text: TextSpan(
+                                            
+                                        //       text: '${API?[index].name}',
+                                        //       style:
+                                        //           TextStyle(color: Colors.black)),
+                                        // ),
+                                        // Text("${API?[index].address}"),
+                                        // Text("${API?[index].province}"),
+                                        // API?[index].phone != null
+                                        //     ? Text("${API?[index].phone}")
+                                        //     : Text("Tidak ada nomor hp"),
+                                        Text("${API?[index].region}",overflow: TextOverflow.ellipsis,style: TextStyle(color: Colors.grey[10]),)
+                                      ],
+                                    ),
+                                  )
                                 ],
                               ),
-                            ),
+                            ],
                           ),
-                        ],
+                        ),
                       );
                     },
                   ),
